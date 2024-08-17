c52f9761297b757df8caf6b7d2e7e535165f0e88
unknown <aniakbennett@gmail.com>
2024-08-17 09:48:04 -0400

    fixing steps to be an array of objects

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggle() {
+  let x = document.getElementById("cert");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+let JSAlgorithms = [
+pyramindGenerator {
+  count: 118,
+  finished: 118
+},
+gradebookApp {
+  count: 4,
+  finished: 4
+},
+rpg {
+  count: 173,
+  finished: 173
+},
+
+const backgroundColorChangerCount = 8;
+let finishedBackgroundColorChanger = 8;
+const calorieCounterCount = 96;
+let finishedCalorieCounter = 75;
+const rpsCount = 6;
+let finishedRps = 6;
+const musicplayerCount = 99;
+let finishedMusicplayer = 20;
+const palindromeCheckerCount = 1;
+let finishedPalindromeChecker = 0;
+];
